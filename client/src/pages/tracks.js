272d148837c6f6d5b25095b64e399d0b718d2fe9
gw.jang <gw.jang@cardoc.kr>
2022-04-21 15:53:38 +0900

    Lift-off I: Basics

--- client/src/pages/tracks.js
+++ client/src/pages/tracks.js
@@ -1,12 +1,38 @@
-import React from 'react';
-import { Layout } from '../components';
+import React from "react";
+import { gql, useQuery } from "@apollo/client";
 
-/**
- * Tracks Page is the Catstronauts home page.
- * We display a grid of tracks fetched with useQuery with the TRACKS query
- */
+import { Layout } from "../components";
+import TrackCard from "../containers/track-card";
+import { QueryResult } from "../components";
+
+export const TRACKS = gql`
+  query getTracks {
+    tracksForHome {
+      id
+      title
+      thumbnail
+      length
+      modulesCount
+      author {
+        name
+        photo
+      }
+    }
+  }
+`;
+
 const Tracks = () => {
-  return <Layout grid> </Layout>;
+  const { loading, error, data } = useQuery(TRACKS);
+
+  return (
+    <Layout grid>
+      <QueryResult loading={loading} error={error} data={data}>
+        {data?.tracksForHome?.map((track) => {
+          return <TrackCard key={track.id} track={track} />;
+        })}
+      </QueryResult>
+    </Layout>
+  );
 };
 
 export default Tracks;
